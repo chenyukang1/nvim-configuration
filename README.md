a6c8eac26c20ed2b6e4c02e9d318997ff811555c
yukang.chen <chen.yukang@qq.com>
2026-02-03 00:16:53 +0800

    Update README

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ build from [dotfiles](https://github.com/hendrikmi/dotfiles)
 mv ~/.config/nvim ~/.config/nvim.bak
 
 rm -rf ~/.local/share/nvim/
+
 rm -rf ~/.local/state/nvim
+
 rm -rf ~/.cache/nvim
 
 git clone git@github.com:chenyukang1/nvim-configuration.git ~/.config/nvim
--- lua/plugins/everforest-theme.lua
+++ lua/plugins/everforest-theme.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   config = function()
     require('everforest').setup {
-      background = 'medium', -- 'hard', 'medium', 'soft'
+      background = 'hard', -- 'hard', 'medium', 'soft'
       -- transparent_background_level = 1, -- 如果你想要透明背景
     }
     vim.cmd [[colorscheme everforest]]
